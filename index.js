3e76a68bb57e58a62ba9b42d089db902a8cc98f6
Rohith Katukam <katukamrohith@gmail.com>
2023-03-15 22:26:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,47 +1,32 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
 
-var numberOfButtons = document.querySelectorAll(".drum").length;
-for(i=0; i<numberOfButtons; i++){
-    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-       var buttonMusic = this.innerHTML;
-       switch (buttonMusic) {
+<head>
+  <meta charset="utf-8">
+  <title>Drum Kit</title>
+  <link rel="stylesheet" href="styles.css">
+  <link href="https://fonts.googleapis.com/css?family=Arvo" rel="stylesheet">
+</head>
 
-         case "w":
-           var crash = new Audio("sounds/crash.mp3");
-           crash.play();
-           break;
+<body>
 
-         case "a":
-           var kick = new Audio("sounds/kick-bass.mp3");
-           kick.play();
-           break;
+  <h1 id="title">Drum 🥁 Kit</h1>
+  <div class="set">
+    <button class="w drum">w</button>
+    <button class="a drum">a</button>
+    <button class="s drum">s</button>
+    <button class="d drum">d</button>
+    <button class="j drum">j</button>
+    <button class="k drum">k</button>
+    <button class="l drum">l</button>
+  </div>
 
-         case "s":
-           var snare = new Audio("sounds/snare.mp3");
-           snare.play();
-           break;
+  <script src="index.js"></script>
+</body>
+<footer>
+  <p>
+    | Designed by Rohith Katukam |
+  </p>
+</footer>
 
-         case "d":
-           var tom1 = new Audio("sounds/tom-1.mp3");
-           tom1.play();
-           break;
-
-         case "j":
-           var tom2 = new Audio("sounds/tom-2.mp3");
-           tom2.play();
-           break;
-
-         case "k":
-           var tom3 = new Audio("sounds/tom-3.mp3");
-           tom3.play();
-           break;
-
-         case "l":
-           var tom4 = new Audio("sounds/tom-4.mp3");
-           tom4.play();
-           break;
-       }
-    });
-}
-
-
-
+</html>
